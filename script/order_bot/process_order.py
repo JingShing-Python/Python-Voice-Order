ddb59e9a0a367e05365221044214ced2cf5a9316
JingShing <JingShingLai@gmail.com>
2022-12-08 02:18:14 +0800

    Add files via upload

--- script/order_bot/process_order.py
+++ script/order_bot/process_order.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import re
-from setting import chinese_to_arabic
+from chinese_num_transform import chinese_to_arabic
 chinese_char_dict = {
     '〇' : 0, '一' : 1, '二' : 2, '三' : 3, '四' : 4, '五' : 5, '六' : 6, '七' : 7, '八' : 8, '九' : 9, '零' : 0,
     '壹' : 1, '贰' : 2, '叁' : 3, '肆' : 4, '伍' : 5, '陆' : 6, '柒' : 7, '捌' : 8, '玖' : 9, '貮' : 2, '兩' : 2,
