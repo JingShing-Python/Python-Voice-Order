eadc2d1b96ed9696bd6caa0382d3a8030267131e
JingShing <JingShingLai@gmail.com>
2022-12-20 13:15:04 +0800

    Add files via upload

--- script/order_bot_program_module_custom/setting.py
+++ script/order_bot_program_module_custom/setting.py
@@ -24,13 +24,22 @@ module_import_dict={
     '幾點':'what_time',
     '時間':'what_time',
 }
+try:
+    from dialogue_section.order_meal import order_meal
+    module_data_dict["餐"]=order_meal
+except:
+    pass
+
+import dialogue_section.order_meal
+
 for key in module_import_dict.keys():
-    exec(
-'''
+    program = '''
 try:
     from dialogue_section.'''+module_import_dict[key]+''' import '''+module_import_dict[key]+'''
     module_data_dict["'''+key+'''"]='''+module_import_dict[key]+'''
 except:
     pass
 '''
-    )
+    exec(program)
+    # print(program)
+print(module_data_dict)
