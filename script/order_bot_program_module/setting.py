ad2f8f909ff88dc2f2357e7511691e42b7affaf1
JingShing <JingShingLai@gmail.com>
2022-12-20 12:25:57 +0800

    Add files via upload

--- script/order_bot_program_module/setting.py
+++ script/order_bot_program_module/setting.py
@@ -3,6 +3,7 @@ command_data_dict = {'command_line':'',
                 'bot_reply':''}
 module_data_dict = dict()
 
+# exit
 try:
     import sys
     module_data_dict['離開']=sys.exit
@@ -10,33 +11,41 @@ try:
 except:
     pass
 
-# import hello
+# dialogue
+# hello
 try:
     from dialogue_section.hello import hello
     module_data_dict['你好']=hello
 except:
     pass
-
-# import my name
+# my name
 try:
     from dialogue_section.my_name import my_name
     module_data_dict['我是']=my_name
     module_data_dict['我的名字是']=my_name
 except:
     pass
-
+# what eat
 try:
     from dialogue_section.what_eat import what_eat
     module_data_dict['吃的']=what_eat
 except:
     pass
-
+# fake rate
 try:
     from dialogue_section.fake_rate import fake_rate
     module_data_dict['機率']=fake_rate
 except:
     pass
+# what time
+try:
+    from dialogue_section.what_time import what_time
+    module_data_dict['幾點']=what_time
+    module_data_dict['時間']=what_time
+except:
+    pass
 
+# calculater
 # order meal
 try:
     from dialogue_section.order_meal import order_meal
@@ -44,9 +53,9 @@ try:
 except:
     pass
 
+# equation
 try:
-    from dialogue_section.what_time import what_time
-    module_data_dict['幾點']=what_time
-    module_data_dict['時間']=what_time
+    from dialogue_section.equation import equation
+    module_data_dict['方']=equation
 except:
     pass
