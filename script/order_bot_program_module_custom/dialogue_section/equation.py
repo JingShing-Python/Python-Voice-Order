ba44ef1e4742a075fe2fbc9c261aa7e18d8bfc16
JingShing <JingShingLai@gmail.com>
2023-01-08 19:57:45 +0800

    Update equation.py

--- script/order_bot_program_module_custom/dialogue_section/equation.py
+++ script/order_bot_program_module_custom/dialogue_section/equation.py
@@ -5,6 +5,8 @@ import re
 #numpy
 import numpy as np
 
+from time import sleep
+
 def equation(self):
     self.line_speaker('請講一下是甚麼題目讓你連一元一次方程式都不會解的？')
     self.line_speaker('先講一下題目有幾個變數？')
@@ -15,9 +17,9 @@ def equation(self):
        self.line_speaker('請再說一次..')
     self.line_speaker('有{}個變數，請說{}個方程式。'.format(var_num,var_num))
     # AX=B
-    A = np.array([[0]*var_num for _ in range(var_num)])
-    B = np.array([[0] for _ in range(var_num)])
-    X = np.array([[None] for _ in range(var_num)])
+    A = np.array([[0]*var_num for _ in range(var_num)], dtype=float)
+    B = np.array([[0] for _ in range(var_num)], dtype=float)
+    X = np.array([[None] for _ in range(var_num)], dtype=float)
     for i in range(var_num):
         while True:
             self.line_speaker('可以開始說了..')
@@ -26,12 +28,12 @@ def equation(self):
             math_line = re.sub(r'[負副富減]+','-',math_line)
             math_line = re.sub(r'等於','=',math_line)
             math_line = re.sub(r'加','+',math_line)
+            math_line = re.sub(r'[立力例]','z',math_line)
             math_line = re.sub(r'(?<=\D)(?=[xyz])|^(?=[xyz])','1',math_line)
-            print(math_line)
-            if len(re.findall(r'([^xyzXYZ\+\-\d=])',math_line))==0:
-                break
-            else:
+            if len(re.findall(r'([^.xyzXYZ\+\-\d=])',math_line))!=0 or math_line[-1]=='=' or not '=' in math_line:
                 self.line_speaker('請再說一次..')
+            else:
+                break
         if var_num >= 1:
             finda = re.findall(r'([\+\-\d]+)x',math_line)
             A[i][0] = int(finda[0]) if len(finda)>=1 else 0
@@ -42,21 +44,27 @@ def equation(self):
             finda = re.findall(r'x?y?([\+\-\d]+)z',math_line)
             A[i][2] = int(finda[0]) if len(finda)>=1 else 0
         findx = re.findall(r'=(.*)',math_line)
-        if len(findx)>=1:
-            B[i][0] = int(findx[0])
-        else:
-            B[i][0]=0
+        try:
+            B[i][0] = float(findx[0])
+        except:
+            B[i][0] = 0
     have_ans=True
     try:
         X = np.linalg.inv(A).dot(B)
     except:
         have_ans=False
     if have_ans:
+        ans = [X[k][0] for k in range(len(X))]
+        for i in range(len(ans)):
+            if ans[i]-int(ans[i])==0.0:
+                ans[i]=int(ans[i])
         if var_num==1:
-            self.line_speaker('X等於{}'.format(X[0][0]))
+            self.line_speaker('X等於{}'.format(ans[0]))
         elif var_num==2:
-            self.line_speaker('X等於{}\nY等於{}'.format(X[0][0],X[1][0]))
+            self.line_speaker('X等於{}\nY等於{}'.format(ans[0],ans[1]))
         else:
-            self.line_speaker('X等於{}\nY等於{}Z等於{}'.format(X[0][0],X[1][0],X[2][0]))
+            self.line_speaker('X等於{}\nY等於{}\nZ等於{}'.format(ans[0],ans[1],ans[2]))
     else:
         self.line_speaker('此題無解or無限解 ^_^')
+    sleep(2)
+    self.line_speaker('很高興為您服務，如果你需要法律諮詢，我們也可以為您解惑')
